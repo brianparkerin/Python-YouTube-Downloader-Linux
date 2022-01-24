2855cb05a805320306ad6f5c8c6a6c30b633679e
Brian Parker <60494113+brianparkerin@users.noreply.github.com>
2022-01-24 08:42:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 from pytube import YouTube, streams
 from pytube.extract import video_info_url
+from progress.bar import Bar, IncrementalBar
 
 
-url = input("Introduce the URL Here:")
+
+url = input("Enter URL Here: ")
 yt = YouTube(url)
 print(yt.title)
-print("Downloading...")
+
+bar = IncrementalBar('Processing', suffix='%(percent)d%%')
+for i in range(100):
+    # Download function with progressive bar en porcentaje 
+    yt.streams.get_highest_resolution().download(r"/home/square/Pictures/Videos") 
+    bar.next()
+bar.finish()
+
+print("The download was finished succesfully!!")
+
+
+# (r"/home/square/Desktop/Downloads")
 
 
 # Example Code
@@ -14,7 +27,6 @@ print("Downloading...")
 #YouTube(url).streams.get_highest_resolution().download(r"/home/square/Downloads")
 
 # Normal Download Functions
-YouTube(url).streams.get_audio_only().download(r"/home/square/Desktop/Music")
-YouTube(url).streams.get_highest_resolution().download(r"/home/square/Videos")
+#YouTube(url).streams.get_audio_only().download(r"/home/square/Desktop/Music")
+#YouTube(url).streams.get_highest_resolution().download(r"/home/square/Videos")
 
-print("The download was finished succesfully!!")
